9f6af979b0c7dd94215bb6982971d6616400a21e
fichkey <39339904+fichkey@users.noreply.github.com>
2026-04-19 19:41:18 -0400

    Change model from llama3-70b-8192 to llama3-8b-8192

--- day2-structured-outputs-tools/day2_agent.py
+++ day2-structured-outputs-tools/day2_agent.py
@@ -1,7 +1,7 @@
 from langchain_openai import ChatOpenAI
 llama3 = ChatOpenAI (api_key = groq_api_key,
                      base_url = "https://api.groq.com/openAI/v1",
-                     model = "llama3-70b-8192",
+                     model = "llama3-8b-8192",
                     )
 llama3
 ai_msg = llama3.invoke ("Hi, how are you?")
