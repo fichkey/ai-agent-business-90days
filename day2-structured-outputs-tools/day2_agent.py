788269575df0f093fd25322ed84dca2507373644
fichkey <39339904+fichkey@users.noreply.github.com>
2026-04-19 19:43:30 -0400

    Fix base URL for ChatOpenAI API

--- day2-structured-outputs-tools/day2_agent.py
+++ day2-structured-outputs-tools/day2_agent.py
@@ -1,6 +1,6 @@
 from langchain_openai import ChatOpenAI
 llama3 = ChatOpenAI (api_key = groq_api_key,
-                     base_url = "https://api.groq.com/openAI/v1",
+                     base_url = "https://api.groq.com/openai/v1",
                      model = "llama3-8b-8192",
                     )
 llama3
