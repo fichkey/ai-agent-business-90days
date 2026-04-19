013e3deb523015b9dd2717f6e080af2b72550bb1
fichkey <39339904+fichkey@users.noreply.github.com>
2026-04-19 19:55:24 -0400

    Update model in day2_agent.py to llama-3.3-70b-versatile

--- day2-structured-outputs-tools/day2_agent.py
+++ day2-structured-outputs-tools/day2_agent.py
@@ -1,7 +1,8 @@
+!pip install langchain_openai
 from langchain_openai import ChatOpenAI
 llama3 = ChatOpenAI (api_key = groq_api_key,
                      base_url = "https://api.groq.com/openai/v1",
-                     model = "llama3-8b-8192",
+                     model = "llama-3.3-70b-versatile",
                     )
 llama3
 ai_msg = llama3.invoke ("Hi, how are you?")
