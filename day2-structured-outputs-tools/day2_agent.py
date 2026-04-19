af5daba34d7b09640ea0d7fcd92587297f0506da
fichkey <39339904+fichkey@users.noreply.github.com>
2026-04-19 19:27:24 -0400

    Import ChatOpenAI and invoke llama3 model

--- day2-structured-outputs-tools/day2_agent.py
+++ day2-structured-outputs-tools/day2_agent.py
@@ -1,1 +1,8 @@
-from langchain_openAI
+from langchain_openai import ChatOpenAI
+llama3 = ChatOpenAI (api_key = groq_api_key,
+                     base_url = "https://api.groq.com/openAI/v1",
+                     model = "llama3-70b-8192",
+                    )
+llama3
+ai_msg = llama3.invoke ("Hi, how are you?")
+print (ai_msg.content)
